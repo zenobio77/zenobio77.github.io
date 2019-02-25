c6287258a983400df7f8c4c6a30e268014f6d32a
Madelon <mad67zen@gmail.com>
2019-02-24 21:52:02 -0300

    Delet DjangoMad - Github dows not support dinamic pages

--- djangoMad/landingpages/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LandingpagesConfig(AppConfig):
-    name = 'landingpages'
--- djangoMad/landingpages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from. import views
-
-urlpatterns = [
-    path('', views.paginaPrincipal, name='pagina-principal'),
-    path('quemSomos', views.quemSomos, name='quem-somos'),
-    path('servicos', views.servicos, name='servicos'),
-]
